3d04c4d9274e3863c2feb49bc9351307f71d088b
mike rumore <mike.a.rumore@gmail.com>
2015-12-30 00:25:45 -0600

    adding new data repo

---
+++ rsrc/angelo_npm_w1_0830/load_data.R
@@ -0,0 +1,53 @@
+#
+sqlite_load_angelo_npm_w1_0830 <- function(nrows=0, at.home=FALSE)
+{
+    Sys.time()
+    #
+    db_path = "/home/MRumore/analytics/db"
+    db_name = "angelo_npm_w1_0830"
+    #
+    if ((at.home) ||
+        (getwd() == "/home/ombt/analytics/rdata/angelo_npm_w1_0830"))
+    {
+        db_path = "/home/ombt/analytics/db"
+        db_name = "angelo_npm_w1_0830"
+    }
+    #
+    Sys.time()
+    db = sqlite_open_db(db_path, db_name)
+    #
+    tbls = c("BRecg",
+             "FILENAME_TO_IDS",
+             "InspectionData",
+             "MountPickupFeeder",
+             "Count",
+             "HeightCorrect",
+             "MountExchangeReel",
+             "MountPickupNozzle",
+             "CycleTime",
+             "Index",
+             "MountLatestReel",
+             "MountQualityTrace",
+             "FID_DATA",
+             "Information",
+             "MountNormalTrace",
+             "Time")
+    #
+    data = list()
+    #
+    Sys.time()
+    for (tbl in tbls)
+    {
+        Sys.time()
+        print(paste("reading table", tbl))
+        data[[tbl]] = sqlite_load_table_from_db(db,
+                                                tbl,
+                                                nrows=nrows)
+    }
+    #
+    Sys.time()
+    sqlite_close_db(db)
+    #
+    Sys.time()
+    return(data)
+}
